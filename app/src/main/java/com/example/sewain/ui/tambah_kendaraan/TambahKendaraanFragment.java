e004c8ac1364e819409004e80bfc407817482238
Vicky Anggara <vickyanggara16@gmail.com>
2019-11-16 10:55:18 +0700

    tambah kendaraan done

--- app/src/main/java/com/example/sewain/ui/tambah_kendaraan/TambahKendaraanFragment.java
+++ app/src/main/java/com/example/sewain/ui/tambah_kendaraan/TambahKendaraanFragment.java
@@ -1,19 +1,24 @@
 package com.example.sewain.ui.tambah_kendaraan;
 
 import android.Manifest;
+import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.provider.Settings;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -31,17 +36,26 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.example.sewain.LoginActivity;
 import com.example.sewain.R;
-import com.google.android.gms.location.FusedLocationProviderClient;
-import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
+
+import static android.graphics.Bitmap.Config.RGB_565;
 
 
 public class TambahKendaraanFragment extends Fragment {
@@ -52,12 +66,15 @@ public class TambahKendaraanFragment extends Fragment {
     private TextInputEditText namaKendaraan, hargaSewa, deskripsiKendaraan;
     private EditText alamatKendaraan;
     private TextView latti, longi;
-    private Button btnKamera, btnTandai;
+    private Button btnKamera, btnTandai, btnSimpan;
     private ImageView gambar;
+    private String userid;
+    private Bitmap foto;
     private AppCompatSpinner spinnerJenis;
 
+    FirebaseAuth mAuth;
 
-    private StorageReference mStorage;
+    private StorageReference mStorageRef;
     private DatabaseReference mReference;
     private ProgressDialog mProgress;
 
@@ -66,30 +83,36 @@ public class TambahKendaraanFragment extends Fragment {
 
     String Latitude = "777";
     String Longitude = "777";
-
     private TambahKendaraanViewModel tambahKendaraanViewModel;
+
+    private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 100;
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         tambahKendaraanViewModel = ViewModelProviders.of(this).get(TambahKendaraanViewModel.class);
 
         View root = inflater.inflate(R.layout.fragment_tambah_kendaraan, container, false);
         final TextView textView = root.findViewById(R.id.text_tambah_kendaraan);
+        mProgress = new ProgressDialog(getContext());
 
         namaKendaraan = root.findViewById(R.id.edtNamaKendaraan);
         hargaSewa = root.findViewById(R.id.edtHargaSewa);
         deskripsiKendaraan = root.findViewById(R.id.edtDeskripsi);
         alamatKendaraan = root.findViewById(R.id.edtAlamatKendaraan);
+        spinnerJenis = root.findViewById(R.id.SpinnerJenis);
 
         latti = root.findViewById(R.id.latti);
         longi = root.findViewById(R.id.longi);
 
         btnKamera = root.findViewById(R.id.ambilFoto);
         btnTandai = root.findViewById(R.id.tandaiLokasi);
+        btnSimpan = root.findViewById(R.id.btnSimpan);
 
         gambar = root.findViewById(R.id.fotoKendaraan);
 
 
-        mStorage = FirebaseStorage.getInstance().getReference();
+        mAuth = FirebaseAuth.getInstance();
+
+        mStorageRef = FirebaseStorage.getInstance().getReference();
 
         mProgress = new ProgressDialog(this.getContext());
 
@@ -110,7 +133,9 @@ public class TambahKendaraanFragment extends Fragment {
         btnKamera.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                // start the image capture Intent
+                startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
             }
         });
 
@@ -132,8 +157,107 @@ public class TambahKendaraanFragment extends Fragment {
         });
 
 
+        btnSimpan.setOnClickListener(view -> {
+
+            String txt_NamaKendaraan = namaKendaraan.getText().toString();
+            String txt_JenisKendaraan = spinnerJenis.getSelectedItem().toString();
+            String txt_HargaSewa = hargaSewa.getText().toString();
+            String txt_AlamatKendaraan = alamatKendaraan.getText().toString();
+            String txt_Latti = latti.getText().toString();
+            String txt_Longi = longi.getText().toString();
+            String txt_DeskripsiKendaraan = deskripsiKendaraan.getText().toString();
+            Bitmap img_Kendaraan = ((BitmapDrawable)gambar.getDrawable()).getBitmap();
+            Bitmap emptyBitmap = Bitmap.createBitmap(gambar.getWidth(), gambar.getHeight(),RGB_565);
+
+            if (TextUtils.isEmpty(txt_NamaKendaraan) || TextUtils.isEmpty(txt_HargaSewa) ||
+                    TextUtils.isEmpty(txt_AlamatKendaraan) || TextUtils.isEmpty(txt_Latti) ||
+                    TextUtils.isEmpty(txt_Longi) || TextUtils.isEmpty(txt_DeskripsiKendaraan) ||
+                    img_Kendaraan.sameAs(emptyBitmap) || TextUtils.isEmpty(txt_JenisKendaraan)) {
+                Toast.makeText(getContext(), "Lengkapi semua field Fergusso!", Toast.LENGTH_SHORT).show();
+            }
+            else {
+                InsertFirebase(txt_NamaKendaraan, txt_JenisKendaraan, txt_HargaSewa, txt_AlamatKendaraan, txt_Latti,
+                        txt_Longi, txt_DeskripsiKendaraan, img_Kendaraan);
+            }
+        });
 
         return root;
+    }
+
+    private void InsertFirebase(String txt_namaKendaraan, String txt_jenisKendaraan, String txt_hargaSewa,
+                                String txt_alamatKendaraan, String txt_latti, String txt_longi,
+                                String txt_deskripsiKendaraan, Bitmap img_kendaraan) {
+
+        mProgress.setMessage("Please Wait");
+        mProgress.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+        mProgress.setCancelable(false);
+        mProgress.setCanceledOnTouchOutside(false);
+        mProgress.setIndeterminate(false);
+        mProgress.show();
+        FirebaseUser firebaseUser = mAuth.getCurrentUser();
+        assert firebaseUser != null;
+        userid = firebaseUser.getUid();
+        String key = FirebaseDatabase.getInstance().getReference("Kendaraan").push().getKey();
+        mReference = FirebaseDatabase.getInstance().getReference("Kendaraan").child(key);
+
+        HashMap<String, String> hashMap = new HashMap<>();
+        hashMap.put("User Id", userid);
+        hashMap.put("Nama Kendaraan", txt_namaKendaraan);
+        hashMap.put("Jenis Kendaraan", txt_jenisKendaraan);
+        hashMap.put("Harga Sewa", txt_hargaSewa);
+        hashMap.put("Alamat Kendaraan", txt_alamatKendaraan);
+        hashMap.put("Lattitude", txt_latti);
+        hashMap.put("Longitude", txt_longi);
+        hashMap.put("Deskripsi Kendaraan", txt_deskripsiKendaraan);
+        hashMap.put("Foto Kendaraan", foto.toString());
+
+        mReference.setValue(hashMap).addOnCompleteListener(task1 -> {
+        if (task1.isSuccessful()) {
+            uploadImage(foto);
+            Intent intent = new Intent(getContext(), LoginActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(intent);
+            Toast.makeText(this.getContext(), "Sukses insert ! " , Toast.LENGTH_SHORT).show();
+         }else{
+            Toast.makeText(this.getContext(), "Gagal insert ! " , Toast.LENGTH_SHORT).show();
+        }
+         mProgress.hide();
+         });
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE && resultCode == Activity.RESULT_OK)
+        {
+            foto = (Bitmap) data.getExtras().get("data");
+            gambar.setImageBitmap(foto);
+        }
+    }
+
+    public void uploadImage(Bitmap bitmap) {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+        byte[] data = baos.toByteArray();
+
+        FirebaseStorage storage = FirebaseStorage.getInstance();
+        String key = FirebaseDatabase.getInstance().getReference("Kendaraan").push().getKey();
+        StorageReference storageRef = storage.getReferenceFromUrl("gs://sewain-fbed3.appspot.com");
+        StorageReference imagesRef = storageRef.child("Kendaraan").child(userid).child(key);
+
+        UploadTask uploadTask = imagesRef.putBytes(data);
+        uploadTask.addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception exception) {
+                // Handle unsuccessful uploads
+            }
+        }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                // taskSnapshot.getMetadata() contains file metadata such as size, content-type, and download URL.
+//                Uri downloadUrl = taskSnapshot.getDownloadUrl();
+                // Do what you want
+            }
+        });
     }
 
     private void getAlamat() {
